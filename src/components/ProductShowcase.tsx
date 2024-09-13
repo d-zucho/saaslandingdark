7641a149695955df6eda2ed502042123c2a54b48
D_Zucho <danny.nico.dev@outlook.com>
2024-09-13 01:23:25 -0700

    add showcase section

--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -1,5 +1,20 @@
-import appScreen from "../assets/images/app-screen.png";
+import Image from 'next/image'
+import appScreen from '../assets/images/app-screen.png'
 
 export const ProductShowcase = () => {
-  return null;
-};
+  return (
+    <section className='bg-black text-white bg-gradient-to-b from-black to-[#5D2CA8] py-[72px] sm:py-24'>
+      <div className='container'>
+        <h2 className='text-center font-bold text-5xl sm:text-6xl tracking-tighter'>
+          Intuitive interface
+        </h2>
+        <p className='text-xl text-center text-white/70 mt-5 max-w-xl mx-auto'>
+          Celebrate the joy of accomplishment with an app designed to track your
+          progress, motivate your efforts, and celebrate your successes, one
+          task at a time.
+        </p>
+        <Image src={appScreen} alt='App screen' className='mt-14 mx-auto' />
+      </div>
+    </section>
+  )
+}
